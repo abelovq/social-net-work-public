d0d72d77107d02397ae42c3aa766a8dc63312b12
leviossa <leviossa@Mac-mini-leviossa.local>
2020-01-27 17:19:46 +0300

    init commit

--- src/index.js
+++ src/index.js
@@ -4,7 +4,36 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import createSagaMiddleware from 'redux-saga';
+import {createStore, applyMiddleware, compose} from 'redux';
+
+import { Provider } from 'react-redux';
+
+import reducer from './store/reducers';
+import rootSaga from './store/sagas';
+import Signup from './pages/Signup/Signup';
+
+const sagaMiddleware = createSagaMiddleware();
+const store = createStore(
+    reducer, 
+    compose(
+        applyMiddleware(sagaMiddleware),
+        window.devToolsExtension ? window.devToolsExtension() : f => f
+    )
+);
+
+sagaMiddleware.run(rootSaga)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route path="/register" component={Signup} />
+            </Switch>
+        </Router>
+    </Provider> , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
